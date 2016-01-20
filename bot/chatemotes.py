39ceb17e9be2a7f31669e16e1d473678ed82a9fc
Lapoozza <jewelmahanta@gmail.com>
2016-01-20 20:35:59 +0530

    extended command prefixes for chat emotes

--- bot/chatemotes.py
+++ bot/chatemotes.py
@@ -1,13 +1,21 @@
+import yaml
 
+# Loading configurations from config.yaml
+with open('../configuration/config.yaml', 'r') as f:
+    doc = yaml.load(f)
+
+prefix = doc['BOT']['command_prefix']
+
+# Chat emotes list starts from here-----------------------------------------------
 async def main(self, message):
-    if message.content.startswith('!kappa'):
+    if message.content.startswith(prefix+'kappa'):
         await self.send_message(message.channel, 'http://i.imgur.com/N5RzfBB.png')
 
-    if message.content.startswith('!feelsbadman'):
+    if message.content.startswith(prefix+'feelsbadman'):
         await self.send_message(message.channel, 'http://i.imgur.com/DfURIfh.png')
 
-    if message.content.startswith('!feelsgoodman'):
+    if message.content.startswith(prefix+'feelsgoodman'):
         await self.send_message(message.channel, 'http://i.imgur.com/9Ggf2vs.png')
 
-    if message.content.startswith('!lapz'):
+    if message.content.startswith(prefix+'lapz'):
         await self.send_message(message.channel, 'http://i.imgur.com/I0Lqf3w.png')
